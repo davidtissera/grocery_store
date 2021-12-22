6fc4f0ddeb271fe908eaf8454ff4865afa37ae7c
David Alejandro Tissera <davidtissera94@gmail.com>
2021-12-22 11:08:03 -0300

    Implementation: Introducing ProductsQuantityTable.tsx component

---
+++ app/src/Pages/BuyerPage/components/ProductsQuantityTable.tsx
@@ -0,0 +1,40 @@
+import Table from "@core/components/Table";
+import { IProductQuantity } from "shared/mocks";
+import {
+  calculateDiscountPrice,
+} from "../helpers/calculateDiscountPrice";
+
+export interface IProductsQuantityTable {
+  products: IProductQuantity[];
+}
+
+export default function ProductsQuantityTable(props: IProductsQuantityTable) {
+  const { products } = props;
+
+  const columns = [
+    { name: "name", header: "Item" },
+    { name: "quantity", header: "Quantity" },
+    {
+      name: "price",
+      header: "Price",
+      Cell: (cellValue: number) => <>{`$${cellValue.toFixed(2)}`}</>
+    }
+  ];
+
+  const rows = products.map((product) => ({
+    ...product,
+    price: calculateDiscountPrice(product)
+  }));
+
+  const totalWithoutDiscount = rows.reduce((prev: number, acum) => prev + acum.cost * acum.quantity, 0);
+  const totalWithDiscount = rows.reduce((prev, acum) => prev + acum.price, 0);
+  const savedPrice = totalWithoutDiscount - totalWithDiscount;
+
+  return (
+    <div>
+      <Table columns={columns} rows={rows} />
+      <div>{`Total price: $${totalWithDiscount.toFixed(2)}`}</div>
+      <div>{`You saved $${savedPrice.toFixed(2)} today.`}</div>
+    </div>
+  );
+}
