1a4979ee24e8534539a4a70a625007de60fba2b8
David Alejandro Tissera <davidtissera94@gmail.com>
2021-12-22 13:22:23 -0300

    Modules: Adding import & exports of BuyerPage components

--- app/src/Pages/BuyerPage/components/index.tsx
+++ app/src/Pages/BuyerPage/components/index.tsx
@@ -1,9 +1,12 @@
 import ShoppingCart, { IShoppingCart } from "./ShoppingCart";
+import ShoppingList, { IShoppingList } from "./ShoppingList";
 
 export {
   ShoppingCart,
+  ShoppingList
 };
 
 export type {
   IShoppingCart,
+  IShoppingList,
 };
