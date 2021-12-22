2c8d825089d72fbaac73aef0c5fcd2e3de7c4288
David Alejandro Tissera <davidtissera94@gmail.com>
2021-12-22 10:27:21 -0300

    Implementation: Introducing 'BuyerPage' component view

--- app/src/Pages/BuyerPage/index.tsx
+++ app/src/Pages/BuyerPage/index.tsx
@@ -1,10 +1,26 @@
 import GroceryPricesTable from "@core/components/GroceryPricesTable";
-import { products } from "shared/mocks";
+import { useState } from "react";
+import { IProduct, products } from "shared/mocks";
+import { simulatePromiseDelay } from "shared/utils";
+import ShoppingCart from "./components/ShoppingCart";
 
 export default function BuyerPage() {
+  const [productsQuantity, setProductsQuantity] = useState<IProduct[]>([]);
+
+  const handleBuyProducts = async (formValues: Record<IProduct["name"], number>) => {
+    await simulatePromiseDelay(false, 2000);
+    const transformProductQuantity = (product: IProduct) => ({
+      ...product,
+      quantity: formValues[product.name],
+    });
+
+    setProductsQuantity(products.map(transformProductQuantity));
+  };
+
   return (
     <>
       <GroceryPricesTable products={products} />
+      <ShoppingCart productsToBuy={products} handleBuyProducts={handleBuyProducts} />
     </>
   );
 }
