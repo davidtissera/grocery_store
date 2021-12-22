12109b62780419fcfa37d49dc621475863f74d09
David Alejandro Tissera <davidtissera94@gmail.com>
2021-12-22 00:18:41 -0300

    Implementation: Introducing GroceryPricesTable.tsx

---
+++ app/src/@core/components/GroceryPricesTable.tsx
@@ -0,0 +1,41 @@
+import Table from "@core/components/Table";
+import { IProduct } from "shared/mocks";
+
+export interface IGroceryPricesTable {
+  products: IProduct[];
+}
+
+export default function GroceryPricesTable(props: IGroceryPricesTable) {
+  const { products } = props;
+
+  const columns = [
+    { name: "name", header: "Item" },
+    { name: "cost", header: "Unit price" },
+    { name: "sale_price", header: "Sale price" }
+  ];
+
+  const transformSalePriceProduct = (product: IProduct) => {
+    const {
+      amount_of_products_with_discount: amount,
+      discount_cost: discount,
+    } = product;
+
+    if (!discount) {
+      return product;
+    }
+
+    return {
+      ...product,
+      sale_price: `${amount} for $${(discount * amount).toFixed(2)}`
+    };
+  };
+
+  const rows = products.map(transformSalePriceProduct);
+
+  return (
+    <>
+      <h1>Local Grocery Store Pricing Table</h1>
+      <Table columns={columns} rows={rows} />
+    </>
+  );
+}
