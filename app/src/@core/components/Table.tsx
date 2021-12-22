9d746c6fa99390545e9b642530e8606a3187d9d5
David Alejandro Tissera <davidtissera94@gmail.com>
2021-12-21 23:50:42 -0300

    Implementation: Introducing Table.tsx component

---
+++ app/src/@core/components/Table.tsx
@@ -0,0 +1,50 @@
+export type IColumn = {
+  name: string;
+  header: string;
+  Cell?: (cellValue: string | number) => React.ReactElement;
+};
+
+export interface ITable {
+  rows: Record<string, string | number>[];
+  columns: IColumn[];
+}
+
+export default function Table(props: ITable) {
+  const { columns, rows } = props;
+
+  if (columns.length === 0 || rows.length === 0) {
+    return null;
+  }
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          {columns.map((column) => {
+            return (
+              <th role="columnheader" key={column.name}>
+                {column.header}
+              </th>
+            );
+          })}
+        </tr>
+      </thead>
+      <tbody>
+        {rows.map((row, idx) => {
+          return (
+            <tr role="row" key={`row-${idx}`}>
+              {columns.map((column) => {
+                const cellValue = row[column.name];
+                return (
+                  <td role="cell" key={`${column.name}-${cellValue}`}>
+                    {column.Cell ? column.Cell(cellValue) : cellValue}
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
