a8bc189a59f44e19f808097d14709173233dfea8
David Alejandro Tissera <davidtissera94@gmail.com>
2021-12-22 10:55:15 -0300

    Implementation: Introducing calculateDiscountPrice.ts

---
+++ app/src/Pages/BuyerPage/helpers/calculateDiscountPrice.ts
@@ -0,0 +1,21 @@
+import { IProductQuantity } from "shared/mocks";
+
+export const calculateDiscountPrice: (product: IProductQuantity) => number = (
+  product
+) => {
+  if (!product.amount_of_products_with_discount || !product.discount_cost) {
+    const price = product.cost * product.quantity;
+
+    return price;
+  }
+
+  const discountProportion = product.quantity / product.amount_of_products_with_discount;
+
+  if (discountProportion % 1 !== 0) {
+    const discountPrice = (product.quantity - 1) * product?.discount_cost;
+
+    return discountPrice + product.cost;
+  }
+
+  return product.quantity * product?.discount_cost;
+};
