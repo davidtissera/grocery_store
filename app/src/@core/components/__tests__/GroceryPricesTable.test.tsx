7e2dc45c5205b93d656a0bc19a4cd354c2d53716
David Alejandro Tissera <davidtissera94@gmail.com>
2021-12-22 00:18:11 -0300

    Implementation: Introducing GroceryPricesTable.test.tsx

---
+++ app/src/@core/components/__tests__/GroceryPricesTable.test.tsx
@@ -0,0 +1,38 @@
+import { screen } from "@testing-library/dom";
+import GroceryPricesTable, { IGroceryPricesTable } from "../GroceryPricesTable";
+import { products } from "shared/mocks";
+import { getComponent } from "shared/testUtils";
+import { IProduct } from "shared/mocks";
+
+describe("GroceryPricesTable", () => {
+  const defaultProps: IGroceryPricesTable = {
+    products: [],
+  };
+  it("should render without crashing", () => {
+    const { container } = getComponent({ Component: GroceryPricesTable, defaultProps });
+
+    expect(container).toBeDefined();
+  });
+
+  it("should render heading title", () => {
+    getComponent({ Component: GroceryPricesTable, defaultProps });
+
+    const heading = screen.getByRole("heading", { name: "Local Grocery Store Pricing Table" });
+
+    expect(heading).toBeInTheDocument();
+  });
+
+  describe("When products are provided", () => {
+    it("should render sale price cells according 'products' prop", () => {
+      const overrideProps: IProduct = {
+        products,
+      };
+      getComponent({ Component: GroceryPricesTable, defaultProps, overrideProps });
+      const milkSalePriceCell = screen.getByRole("cell", { name: "2 for $5.00" });
+      const breadSalePriceCell = screen.getByRole("cell", { name: "3 for $6.00" });
+  
+      expect(milkSalePriceCell).toBeInTheDocument();
+      expect(breadSalePriceCell).toBeInTheDocument();
+    });
+  });
+});
